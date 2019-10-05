7da4d6b4e5bba8cdea724e4893023d58e19b088b
Jimmy Bergström <thekwarf@gmail.com>
2019-10-05 02:23:12 +0200

    Dropped Tiled in favor of Godots built in map support
    I missed the auto-tiling that Godot has too much

--- res/tileset.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="tileset.png" width="512" height="512"/>
- <tile id="0">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
-</tileset>
